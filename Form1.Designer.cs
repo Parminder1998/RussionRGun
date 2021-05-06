a936c64b75d55c58d7958b7ac4e9e15e5e0d2d37
Karan2824 <81338532+Parminder1998@users.noreply.github.com>
2021-05-07 08:47:06 +1200

    Parminder

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace RUSSIONRGun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPress = new System.Windows.Forms.Button();
+            this.buttonSpin = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.pictureProcess = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureProcess)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPress
+            // 
+            this.buttonPress.BackColor = System.Drawing.Color.Transparent;
+            this.buttonPress.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonPress.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPress.ForeColor = System.Drawing.Color.Black;
+            this.buttonPress.Location = new System.Drawing.Point(518, 345);
+            this.buttonPress.Name = "buttonPress";
+            this.buttonPress.Size = new System.Drawing.Size(353, 85);
+            this.buttonPress.TabIndex = 15;
+            this.buttonPress.Text = " Trigger";
+            this.buttonPress.UseVisualStyleBackColor = false;
+            this.buttonPress.Click += new System.EventHandler(this.buttonPress_Click);
+            // 
+            // buttonSpin
+            // 
+            this.buttonSpin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonSpin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSpin.Location = new System.Drawing.Point(518, 174);
+            this.buttonSpin.Name = "buttonSpin";
+            this.buttonSpin.Size = new System.Drawing.Size(353, 86);
+            this.buttonSpin.TabIndex = 14;
+            this.buttonSpin.Text = "Spin Chamber";
+            this.buttonSpin.UseVisualStyleBackColor = true;
+            this.buttonSpin.Click += new System.EventHandler(this.buttonSpin_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLoad.Location = new System.Drawing.Point(518, 25);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(353, 89);
+            this.buttonLoad.TabIndex = 13;
+            this.buttonLoad.Text = "Load Gun";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.buttonStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonStart.Location = new System.Drawing.Point(49, 140);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(269, 78);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "Start ";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Tw Cen MT Condensed", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.Location = new System.Drawing.Point(42, 221);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(282, 41);
+            this.lblMsg.TabIndex = 11;
+            this.lblMsg.Text = "Click To Start The Game...";
+            // 
+            // pictureProcess
+            // 
+            this.pictureProcess.Location = new System.Drawing.Point(361, 149);
+            this.pictureProcess.Name = "pictureProcess";
+            this.pictureProcess.Size = new System.Drawing.Size(118, 111);
+            this.pictureProcess.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureProcess.TabIndex = 10;
+            this.pictureProcess.TabStop = false;
+            this.pictureProcess.Click += new System.EventHandler(this.pictureProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(899, 450);
+            this.Controls.Add(this.buttonPress);
+            this.Controls.Add(this.buttonSpin);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.pictureProcess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureProcess)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonPress;
+        private System.Windows.Forms.Button buttonSpin;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.PictureBox pictureProcess;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using RUSSIONRGun.GameEngine;
+using RUSSIONRGun.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RUSSIONRGun
+{
+    public partial class Form1 : Form
+    {
+
+        Bullets Bullets;
+        SoundPlayer player;
+        int chance, Bulletposition, angle;
+        Random random;
+        Timer timer;
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (buttonStart.Text.Contains("Start"))
+            {
+                buttonStart.Text = "End Game";
+                lblMsg.Text = "Click To Load the Bullet...";
+                chance = 3;
+                Bullets = new Bullets();
+                pictureProcess.Visible = true;
+
+                buttonLoad.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Quit The Game...?", "Alert");
+                this.Close();
+            }
+        }
+
+        private async void buttonSpin_Click(object sender, EventArgs e)
+        {
+
+            buttonSpin.Enabled = false;
+            lblMsg.Text = "Spinning the Chamber...";
+            Bullets.Spin();
+            angle = 0;
+            timer = new Timer();
+            timer.Interval = 1;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+           await Task.Delay(2500);
+            timer.Stop();
+
+            buttonPress.Enabled = true;
+            ChangePicture(pictureProcess, Resources.gunwithoutfire);
+            lblMsg.Text = "Click Press Trigger to Fire Bullet...";
+         
+            Bulletposition = 0;
+        }
+
+        private void ChangePicture(PictureBox pictureBox, Bitmap image)
+        {
+            pictureBox.Image = image;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            angle += 20;
+            if (angle == 360)
+            {
+                angle = 0;
+            }
+            Image image = new Bitmap(Resources.withbullet);
+            //Store our old image so we can delete it
+            Image oldImage = pictureProcess.Image;
+            pictureProcess.Image = GameUtility.RotateBulletChamberImage(image, new Point(), angle);
+            if (oldImage != null)
+            {
+                oldImage.Dispose();
+            }
+        }
+
+        private void buttonPress_Click(object sender, EventArgs e)
+        {
+            player = new System.Media.SoundPlayer();
+            if (Bullets.FireBullet(Bulletposition))
+            {
+                player.Stream = Resources.GunShot;
+                player.Play();
+              //  ChangePicture(pictureProcess, Resources.gunwithfire);
+                //ChangePicture(pictureResult, Resources.dead);
+                //await Task.Delay(900);
+                //lblRemaining.Text = "You are Dead!!!";
+                MessageBox.Show("You Loose...", "Alert");
+                this.Close();
+                return;
+            }
+            else
+            {
+                player.Stream = Resources.Empty;
+                player.Play();
+               // await Task.Delay(900);
+            }
+            Bulletposition++;
+            chance--;
+            if (chance == 0)
+            {
+               // lblRemaining.Text = "Thank God! You Are Lucky! You Survived!!!";
+                //ChangePicture(pictureResult, Resources.lucky);
+                //await Task.Delay(400);
+                MessageBox.Show("You Made it!! You Won The Game!!!", "Congratulations");
+                this.Close();
+            }
+            else
+            {
+               // lblRemaining.Text = "Remaining Chance: " + chance;
+            }
+        }
+
+        private async void buttonLoad_Click(object sender, EventArgs e)
+        {
+
+            buttonLoad.Enabled = false;
+            Bulletposition = random.Next(0, 6);
+
+            player = new System.Media.SoundPlayer();
+            player.Stream = Resources.LoadBUllet;
+            player.Play();
+
+            lblMsg.Text = "Loading The Bullet at : " + (Bulletposition + 1);
+            Bullets.LoadBullet(Bulletposition);
+            await Task.Delay(800);
+            int angle = (Bulletposition + 1) * 30;
+            Image image = new Bitmap(Resources.withbullet);
+            //Store our old image so we can delete it
+            Image oldImage = pictureProcess.Image;
+            //Pass in our original image and return a new image rotated 35 degrees right
+            pictureProcess.Image = GameUtility.RotateBulletChamberImage(image, new Point(), angle);
+            if (oldImage != null)
+            {
+                oldImage.Dispose();
+            }
+            lblMsg.Text = "Click to Spin it...";
+            buttonSpin.Enabled = true;
+        }
+
+        private void pictureProcess_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonLoad.Enabled = buttonPress.Enabled = buttonSpin.Enabled = false;
+            random = new Random();
+            pictureProcess.Visible = false;
+        }
+
+        private async Task buttonLoad_ClickAsync(object sender, EventArgs e)
+        {
+
+            buttonLoad.Enabled = false;
+            Bulletposition = random.Next(0, 6);
+
+            player = new System.Media.SoundPlayer();
+            player.Stream = Resources.LoadBUllet;
+            player.Play();
+
+            lblMsg.Text = "Loading The Bullet at : " + (Bulletposition + 1);
+            Bullets.LoadBullet(Bulletposition);
+            await Task.Delay(800);
+            int angle = (Bulletposition + 1) * 30;
+            Image image = new Bitmap(Resources.withbullet);
+            //Store our old image so we can delete it
+            Image oldImage = pictureProcess.Image;
+            //Pass in our original image and return a new image rotated 35 degrees right
+            pictureProcess.Image = GameUtility.RotateBulletChamberImage(image, new Point(), angle);
+            if (oldImage != null)
+            {
+                oldImage.Dispose();
+            }
+            lblMsg.Text = "Click to Spin it...";
+            buttonSpin.Enabled = true;
+        }
+    }
+}
